351e7f136f7cf389244ce21c5eaea323d8ef2f5b
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-05-31 13:09:48 -0500

    mobile menu

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import '../styles/modern-normalize.css'
-import '../styles/style.css'
-import '../styles/utils.css'
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/process.css';
+import '../styles/components/projects.css';
+import '../styles/components/skills.css';
+import '../styles/components/about.css';
+import '../styles/components/contact.css';
+import '../styles/utils.css';
 
